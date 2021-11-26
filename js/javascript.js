d93f0d8e0041630ae624ea3ca7391190cbd81149
João Carlos <joocarlosanjosbrito@gmail.com>
2021-11-25 21:52:37 -0300

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,36 @@
+
+
+function calcular() {
+    let select = parseInt(document.getElementById('seleciona-temperatura').value);
+    // console.log(select);
+    let temperatura = document.getElementById('temperatura').value;
+    let formulaF = (parseInt(temperatura) * 1.8) + 32;
+    let formulak = parseInt(temperatura) + 273;
+    let resultado = document.getElementById('resultado');
+    
+    // if(select === 0){
+    //     resultado.innerHTML = (`${temperatura}°C em Kelvin é ${formulak}K`);
+    // } else if(select == 1){
+    //     resultado.innerHTML = (`${temperatura}°C em Fahrenheit é ${formulaF}°F`)
+    // }
+
+    switch (select){
+        case 0:
+            resultado.innerHTML = (`${temperatura}°C em Kelvin é ${formulak}k`);
+        break;
+
+        case 1:
+            resultado.innerHTML = (`${temperatura}°C em Fahrenheit é ${formulaF}°F`);
+
+        break; 
+        
+        default:
+            resultado.innerHTML = ('Erro');
+
+        break;
+
+
+    }
+    // console.log(temperatura);
+    // resultado.innerHTML = ('O valor convertido é de ' + calculo + '°F');
+}
